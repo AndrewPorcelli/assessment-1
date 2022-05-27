a97fbf09d765000f2f4a6406be39b46ce44ec531
Andrew Porcelli <andy.porcelli1@gmail.com>
2022-05-27 10:35:07 -0600

    Completed the gitInfo.js section

--- gitInfo.js
+++ gitInfo.js
@@ -11,6 +11,9 @@
 
 //CODE HERE
 
+let gitDefinition =
+  "Git is a local repository and version control system, where we can save local files at certain times during our coding, check the status of changes, look at history. Its basically a way to make sure you have files saved, can go through and start code at different save points as well, and use in function with gitHub so that it can be stored on a cloud server.";
+
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable called 'gitHubDefinition'.  
@@ -18,6 +21,9 @@
 */
 
 //CODE HERE
+
+let gitHubDefinition =
+  "GitHub is an online cloud based server that can link to your local git repository so that if your computer dies, your files that are backed up and saved to the github cloud will always be there. It also allows you to share and collaborate with others online with your code, projects or for work as well.";
 
 //////////////////PROBLEMS 3 - 8////////////////////
 /*
@@ -35,6 +41,12 @@
 
 //CODE HERE
 
+const init = {
+  description:
+    "Init is a way to initialize a local repository in the folder you are currently in",
+  code: "git init",
+};
+
 //////////////////PROBLEM 4////////////////////
 /*
     Create an object called 'clone' with 'description' and 'code' properties 
@@ -42,6 +54,12 @@
 */
 
 //CODE HERE
+
+const clone = {
+  description:
+    "A command where you can clone a repository into a new directory and creates remote tracking for each branch within.",
+  code: "git clone",
+};
 
 //////////////////PROBLEM 5////////////////////
 /*
@@ -51,6 +69,12 @@
 
 //CODE HERE
 
+const status = {
+  description:
+    "A way to ask about a repo. It will tell you if a file has been edited since the last commit or before its first initial commit, it will be red. It will tell you if you have added the repos updated parts in the file with the status in green. Then, after committing the repo it should say the branch is clean.",
+  code: "git status",
+};
+
 //////////////////PROBLEM 6////////////////////
 /*
     Create an object called 'add' with 'description' and 'code' properties 
@@ -58,6 +82,12 @@
 */
 
 //CODE HERE
+
+const add = {
+  description:
+    "Add is a way to add files to be tracked always. After you add the files the status should show green in it.",
+  code: "git add . (when you want all the files within the folder to be tracked) || git add (file name) for only that file",
+};
 
 //////////////////PROBLEM 7////////////////////
 /*
@@ -67,6 +97,11 @@
 
 //CODE HERE
 
+const commit = {
+  description:
+    'Commit creates a snapshot of the files that you are currently tracking. It is basically a save point or check point that you can see history in or go back to. You do have to add the -m "" or you will be in noob purgetory.',
+  code: 'git commit -m ""',
+};
 
 //////////////////PROBLEM 8////////////////////
 /*
@@ -75,3 +110,8 @@
 */
 
 //CODE HERE
+
+const push = {
+    description: 'Updates remote refs alond with associated objects. It is used to uplaod local repositries to a remote repository.',
+    code: 'git push -u origin master (or whatever else it has been named)'
+}
